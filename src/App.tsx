fe20b7943c81c660242fef86f96cb1ebb335adf1
ShinobiKoda <adebiyipacsc2022@futa.edu.ng>
2025-12-09 17:59:26 +0100

    feat: add @vercel/analytics for improved analytics tracking in the application

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,13 @@ import Home from "./components/Home";
 import Contact from "./components/Contact";
 import Projects from "./components/Projects";
 import { Routes, Route } from "react-router-dom";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App() {
   return (
     <ThemeProvider>
       <ParticlesAnimation />
+      <Analytics />
       <div className="w-full h-screen dark:bg-black bg-[#222831]">
         <Routes>
           <Route path="/" element={<Home />} />
