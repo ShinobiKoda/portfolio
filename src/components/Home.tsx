9b2aa73fc54d5f462e0ccaf60e399b98aabde02f
ShinobiKoda <adebiyipacsc2022@futa.edu.ng>
2025-12-12 14:23:08 +0100

    feat(home): animate hero content and made download cv functional

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,10 +1,18 @@
 import Navbar from "../components/Layout/Navbar";
+import { FaGithub, FaTwitter, FaLinkedin } from "react-icons/fa";
 import { NavLink } from "react-router-dom";
-import { FaFacebookF } from "react-icons/fa6";
-import { FaLinkedinIn } from "react-icons/fa";
-import { FaXTwitter } from "react-icons/fa6";
-import { BiLogoGmail } from "react-icons/bi";
-import { FaGithub } from "react-icons/fa";
+import {
+  motion,
+  heroContainerVariants,
+  heroItemVariants,
+  socialListVariants,
+  socialItemVariants,
+  iconHover,
+  bounceDropVariants,
+  buttonHover,
+  tapPress,
+  tapTransition,
+} from "./animations/motion";
 
 const Home = () => {
   const handleDownloadCV = () => {
@@ -15,26 +23,12 @@ const Home = () => {
     link.click();
     document.body.removeChild(link);
   };
-  const links = [
+  const socials = [
+    { link: "https://github.com/ShinobiKoda", icon: <FaGithub size={32} /> },
+    { link: "https://x.com/sirp_xo", icon: <FaTwitter size={32} /> },
     {
-      icon: <FaFacebookF size={20} />,
-      link: "https://www.facebook.com/share/17VyDNkpz6/",
-    },
-    {
-      icon: <FaXTwitter size={20} />,
-      link: "https://x.com/sirp_xo",
-    },
-    {
-      icon: <FaLinkedinIn size={20} />,
-      link: "https://www.linkedin.com/in/praise-adebiyi-b8bb72287",
-    },
-    {
-      icon: <BiLogoGmail size={20} />,
-      link: "https://mail.google.com/mail/?view=cm&fs=1&to=sirp2804@gmail.com",
-    },
-    {
-      icon: <FaGithub size={20} />,
-      link: "https://github.com/ShinobiKoda",
+      link: "https://www.linkedin.com/in/praise-adebiyi-b8bb72287/",
+      icon: <FaLinkedin size={32} />,
     },
   ];
 
@@ -42,16 +36,96 @@ const Home = () => {
     <div className="w-full h-full">
       <Navbar />
 
-      <div className="w-full px-4">
-        <h1 className="font-semibold text-[32px] text-white">
-          Praise is a
-          <span className="text-(--text-primary)"> front-end developer</span>
-        </h1>
-        <p className="text-(--text-gray) text-base font-normal mt-[25px]">He crafts responsive websites where technologies meet creativity</p>
-      </div>
+      <motion.div
+        className="absolute top-0 left-[17px] xl:flex flex-col items-center gap-8 hidden"
+        variants={bounceDropVariants}
+        initial="initial"
+        animate="animate"
+      >
+        <motion.div
+          className="w-px h-[191px] bg-(--text-gray)"
+          variants={heroItemVariants}
+        />
+        <motion.ul
+          className="flex flex-col gap-2"
+          variants={socialListVariants}
+          initial="initial"
+          animate="animate"
+        >
+          {socials.map((social, index) => (
+            <motion.li
+              key={index}
+              variants={socialItemVariants}
+              whileHover={iconHover}
+              className="text-(--text-gray)"
+            >
+              <NavLink to={social.link}>{social.icon}</NavLink>
+            </motion.li>
+          ))}
+        </motion.ul>
+      </motion.div>
 
-      <div className="px-4 mt-6 w-full">
+      <div className=" w-full px-4 lg:px-8 max-w-5xl mx-auto space-y-6 lg:flex lg:mt-[62px] lg:items-center">
+        <motion.div
+          className="w-full space-y-[25px] lg:space-y-8"
+          variants={heroContainerVariants}
+          initial="initial"
+          animate="animate"
+        >
+          <motion.h1
+            className="font-semibold text-[32px] text-white"
+            variants={heroItemVariants}
+          >
+            Praise is a
+            <span className="text-(--text-primary)"> front-end developer</span>
+          </motion.h1>
+          <motion.p
+            className="text-(--text-gray) text-base font-normal"
+            variants={heroItemVariants}
+          >
+            He crafts responsive websites where technologies meet creativity
+          </motion.p>
 
+          <motion.button
+            className="hidden lg:block px-4 py-2 border border-(--text-primary) font-medium text-base cursor-pointer text-white"
+            variants={heroItemVariants}
+            whileHover={buttonHover}
+            whileTap={{ ...tapPress, transition: tapTransition }}
+            onClick={handleDownloadCV}
+          >
+            Download CV
+          </motion.button>
+        </motion.div>
+
+        <motion.div
+          className="px-4 w-full max-w-[469px] mx-auto"
+          variants={heroContainerVariants}
+          initial="initial"
+          animate="animate"
+        >
+          <motion.div className="w-full" variants={heroItemVariants}>
+            <motion.img
+              src="/images/hero-img.webp"
+              alt="Hero Image"
+              className="w-full"
+              variants={heroItemVariants}
+            />
+          </motion.div>
+          <motion.div
+            className="p-2 border border-(--text-gray) flex items-center gap-[13px]"
+            variants={heroItemVariants}
+          >
+            <div className="h-4 w-4 bg-(--text-primary)"></div>
+            <p className="flex flex-col">
+              <span className="font-medium text-base text-(--text-gray)">
+                Currently working on
+              </span>
+              <span className="font-semibold text-base text-white">
+                Portfolio
+              </span>
+            </p>
+          </motion.div>
+        </motion.div>
       </div>
     </div>
   );
